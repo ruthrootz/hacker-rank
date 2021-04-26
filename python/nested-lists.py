3120be0b8be0c0d428d114733a4231e3ed9655f6
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-04-26 15:23:39 -0400

    Create nested-lists.py

---
+++ python/nested-lists.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    records = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        records.append([name, score])
+        
+    print(records)
