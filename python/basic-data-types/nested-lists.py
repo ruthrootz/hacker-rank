ab571082fcdae3efbd6e08721eec26bd679a0e4d
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-04-26 15:58:18 -0400

    Update nested-lists.py

--- python/basic-data-types/nested-lists.py
+++ python/basic-data-types/nested-lists.py
@@ -1,1 +1,24 @@
+if __name__ == '__main__':
 
+    records = []
+    second_lowest_score = 1000
+    lowest_score = 999
+    result_names = []
+
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        if score < lowest_score:
+            second_lowest_score = lowest_score
+            lowest_score = score
+        if score < second_lowest_score and score > lowest_score:
+            second_lowest_score = score
+        records.append([name, score])
+
+    for record in records:
+        if record[1] == second_lowest_score:
+            result_names.append(record[0])
+
+    result_names.sort()
+    for name in result_names:
+        print(name)
