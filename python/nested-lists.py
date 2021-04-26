eb5fc5a4627a12a86ae4e55f7b751ac15c3ded2a
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-04-26 15:37:50 -0400

    Update nested-lists.py

--- python/nested-lists.py
+++ python/nested-lists.py
@@ -1,8 +1,10 @@
 if __name__ == '__main__':
+
     records = []
     second_lowest_score = 1000
     lowest_score = 999
-    
+    result_names = []
+
     for _ in range(int(input())):
         name = input()
         score = float(input())
@@ -12,7 +14,11 @@ if __name__ == '__main__':
         if score < second_lowest_score and score > lowest_score:
             second_lowest_score = score
         records.append([name, score])
-        
+
     for record in records:
         if record[1] == second_lowest_score:
-            print(record[0])
+            result_names.append(record[0])
+
+    result_names.sort()
+    for name in result_names:
+        print(name)
