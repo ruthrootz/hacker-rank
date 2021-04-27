32098d92fd7c9766e6072456eed264cb75993e28
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-04-27 13:45:57 -0400

    Update lists.py

--- python/basic-data-types/lists.py
+++ python/basic-data-types/lists.py
@@ -1,3 +1,23 @@
 if __name__ == '__main__':
 
     N = int(input())
+
+    for _ in range(0, N):
+        command = input()
+        split_command = command.split(' ')
+        if split_command[0] == 'print':
+            print('print')
+        elif split_command[0] == 'append':
+            print('append')
+        elif split_command[0] == 'remove':
+            print('remove')
+        elif split_command[0] == 'sort':
+            print('sort')
+        elif split_command[0] == 'pop':
+            print('pop')
+        elif split_command[0] == 'reverse':
+            print('reverse')
+        elif split_command[0] == 'insert':
+            print('insert')
+        else:
+            print('error')
